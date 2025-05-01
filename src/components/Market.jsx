67ef190a6b064c4e7fb66b98b2616404197b3810
Yersain Castaño <yercas93@gmail.com>
2025-04-30 19:24:24 -0500

    market card's shadow style was changed

--- src/components/Market.jsx
+++ src/components/Market.jsx
@@ -12,7 +12,7 @@ const Market = ({ market }) => {
   console.log(market);
 
   return (
-    <div className="shadow-md rounded-lg p-4 px-15 md:px-30 my-6">
+    <div className="shadow-xl rounded-lg p-4 px-15 md:px-30 my-6">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-4">
         {market.map((item) => (
           <div
